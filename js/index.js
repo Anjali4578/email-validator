5d792716617e7e47918ba540d547d4d9aab18dab
Anjali <86060577+Anjali4578@users.noreply.github.com>
2023-09-18 02:35:27 +0530

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,39 @@
+console.log("This is my script");
+
+let result = {
+  tag: "",
+  free: true,
+  role: false,
+  user: "akshaykumar",
+  email: "akshaykumar@gmail.com",
+  score: 0.48,
+  state: "undeliverable",
+  domain: "gmail.com",
+  reason: "invalid_mailbox",
+  mx_found: true,
+  catch_all: null,
+  disposable: false,
+  smtp_check: false,
+  did_you_mean: "",
+  format_valid: true,
+};
+
+submitBtn.addEventListener("click", async (e) => {
+  e.preventDefault()
+  console.log("Clicked!")
+  resultCont.innerHTML = `<img width = "123" src="img/loading.svg" alt="">`
+  let key = "ema_live_AiWpNgpnHvey1D0qIhRNqg2Ykp2LwIgoJTPhFsqw"
+  let email = document.getElementById("username").value
+  let url = `https://api.emailvalidation.io/v1/info?apikey=${key}&email=${email}`
+  let res = await fetch(url)
+  let result = await res.json()
+  let str = ``
+  for (key of Object.keys(result)) {
+    if (result[key] !== "" && result[key] !== " ") {
+        str = str + `<div>${key}: ${result[key]}</div>`
+    }
+  }
+
+  console.log(str)
+  resultCont.innerHTML = str
+});
